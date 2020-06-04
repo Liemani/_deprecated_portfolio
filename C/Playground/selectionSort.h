f953b944b2daecb2cc7273f2956a2a51166167c9
Liemani <parkjh2768@naver.com>
2020-06-04 11:12:49 +0900

    now selectionSort.h use swap()

--- C/Playground/selectionSort.h
+++ C/Playground/selectionSort.h
@@ -30,9 +30,7 @@ void selectionSort(void* array, int count, int sizeOfElement, int (*compare)(voi
 	for (int i = 0; i < sizeOfElement * (count - 1); i += sizeOfElement) {
 		for (int j = i + sizeOfElement; j < sizeOfElement * count; j += sizeOfElement) {
 			if (compare(&charArray[i], &charArray[j], sizeOfElement)) {
-				int temp = charArray[i];
-				charArray[i] = charArray[j];
-				charArray[j] = temp;
+				swap(&charArray[i], &charArray[j], sizeOfElement);
 			}
 		}
 	}
