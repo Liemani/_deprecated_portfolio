872d328cfc3cb1b89381e1adae8de2c163c44295
Liemani <parkjh2768@naver.com>
2020-06-05 18:02:14 +0900

    update parameter data type from pointer to not

--- C/Playground/add_Byte.h
+++ C/Playground/add_Byte.h
@@ -18,8 +18,8 @@
 
 
 
-int add_Byte(unsigned char* lhs, unsigned char* rhs, unsigned char* result) {
-	unsigned short carry = (unsigned short)*lhs + (unsigned short)*rhs;
+int add_Byte(unsigned char lhs, unsigned char rhs, unsigned char carry, unsigned char* result) {
+	unsigned short carry = (unsigned short)lhs + (unsigned short)rhs + carry;
 	*result = (unsigned char)carry;
 
 	return (int)(carry >> 8);
