7e2d5c17a32b466a8b61b080a80f15635e2e0e65
Liemani <49893103+Liemani@users.noreply.github.com>
2020-06-11 19:38:55 +0900

    added head comment

--- Arduino/ex_dht_02/ex_dht_02.ino
+++ Arduino/ex_dht_02/ex_dht_02.ino
@@ -1,3 +1,15 @@
+//********************************************
+// char* title = "ex_dht_02"
+// made by Lieman at 2020.06.11
+//
+// description:
+//  dht example
+//********************************************
+
+
+
+
+
 #include <LiquidCrystal.h>
 #include "DHT.h"
 
