9033ee21a64c95e3a34b20259768a8948e3a4021
Liemani <parkjh2768@naver.com>
2020-06-23 17:29:51 +0900

    Updated what i studied today and yesterday

---
+++ C/TCP_IP/sample/03_endiannessConversion_server/03_endiannessConversion_server.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : 03_endiannessConversion_server.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+int main(int argc, char** argv) {
+	unsigned short host_port = 0x1234;
+	unsigned short net_port;
+	unsigned long host_addr = 0x12345678;
+	unsigned long net_addr;
+
+	net_port = htons(host_port);
+	net_addr = htonl(host_addr);
+
+	printf("Host ordered port: %#x\n", host_port);
+	printf("Network ordered port: %#x\n", net_port);
+	printf("Host ordered address: %#lx\n", host_addr);
+	printf("Network ordered address: %#lx\n", net_addr);
+
+	return EXIT_SUCCESS;
+}
---
+++ C/TCP_IP/sample/README.md
@@ -0,0 +1,3 @@
+# sample folder
+- This code is sample code for TCP/IP programming
+- I didn't make this code
