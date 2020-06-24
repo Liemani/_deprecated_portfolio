ac6d2f853ddc33a3d4115920ed9fd77b1033b4dc
Liemani <parkjh2768@naver.com>
2020-06-25 08:37:32 +0900

    Updated

--- CPP/Playground/playground.cpp
+++ CPP/Playground/playground.cpp
@@ -7,6 +7,11 @@
 //********************************************
 #include <iostream>
 #include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
 
 void helloWorld() {
 	std::cout << std::setiosflags(std::ios::left);
@@ -49,9 +54,48 @@ void getInput2() {
 	std::cout << "All done." << std::endl;
 }
 
+void output1() {
+	cout << "1\t2\t3\t4\t5\t6\t7\t8\t9" << endl << "" << endl;
+	for (int c = 1; c < 10; c++) {
+		cout << c << "|";
+		for (int i = 1; i < 10; ++i) {
+			cout << i * c << '\t';
+		}
+		cout << endl;
+	}
+}
+
+void highLow() {
+	int number;
+	int guess;
+
+	srand(time(NULL));
+
+	int number = rand() % 100 + 1;
+	int guess = 0;
+
+	do {
+		cout << "Guess our number (1 to 100): ";
+
+		if (!(cin >> guess)) {
+			cout << "Invalid Input!" << endl;
+			cin.clear();
+			cin.ignore(10000, '\n');
+		} else {
+			if (guess > number) {
+				cout << guess << " is high" << endl;
+			} else if (guess < number) {
+				cout << guess << " is low" << endl;
+			} else {
+				cout << "Congratulations!" << endl;
+				break;
+			}
+		}
+	} while (true);
+}
 
 int main() {
-	getInput2();
+	highLow();
 
 	return 0;
 }
