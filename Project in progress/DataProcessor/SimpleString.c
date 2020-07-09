be196d797f1017e7df7b692de0d9b4ba01bd3f15
Liemani <parkjh2768@naver.com>
2020-07-09 20:24:38 +0900

    Rename initializer. Create method.

--- Project in progress/DataProcessor/SimpleString.c
+++ Project in progress/DataProcessor/SimpleString.c
@@ -1,7 +1,7 @@
 #pragma once
 //********************************************
 // char* title = "SimpleString.c"
-// made by Lieman at 2020.07.08
+// made by Lieman at 2020.07.09
 //
 // description:
 //	SimpleString implementation
@@ -12,16 +12,117 @@
 
 
 // preprocessor
-#include <stdlib.h>		// malloc()
-#include <string.h>		// strcpy()
+#include <stdlib.h>		// malloc(), realloc(), free()
+#include <string.h>		// strcpy(), strlen(), strcat()
+#include <assert.h>		// assert()
 #include "SimpleString.h"
+#pragma warning(disable:4996) //strcpy()
+
+
+
+
+
+// static variable
+static const int ALLOC_INTERVAL = 8 * sizeof(int);
 
 
 
 
 
 // static method
+int SimpleString__reallocIfNeed(SimpleString* string, int countDelta) {
+	int afterChunk = (string->count + 1 + countDelta) / ALLOC_INTERVAL + 1;
+
+	if (string->chunk != afterChunk) {
+		string->string = (char*)realloc(string->string, ALLOC_INTERVAL * afterChunk);
+		assert(string->string);
+		string->chunk = afterChunk;
+	}
+}
+
 // method
+void SimpleString__append__Character(SimpleString* string, char character) {
+	SimpleString__reallocIfNeed(string, 1);
+	string->string[string->count] = character;
+	string->string[string->count + 1] = '\0';
+
+	++string->count;
+}
+
+void SimpleString__append__String(SimpleString* simpleString, char* string) {
+	int stringCount = strlen(string);
+
+	SimpleString__reallocIfNeed(simpleString, stringCount);
+	strcat(simpleString->string, string);
+
+	simpleString->count += stringCount;
+}
+
+void SimpleString__append__SimpleString(SimpleString* lhs, SimpleString* rhs) {
+	SimpleString__reallocIfNeed(lhs, rhs->count);
+	strcat(lhs->string, rhs->string);
+
+	lhs->count += rhs->count;
+}
+
+void SimpleString__append__hex__fromUCharacter(SimpleString* string, unsigned char character) {
+	char hexArray[3];
+
+	char hex = character >> 4;
+	hexArray[0] = hex + (hex < 10 ? '0' : 'A' - 10);
+
+	hex = character & 0x0F;
+	hexArray[1] = hex + (hex < 10 ? '0' : 'A' - 10);
+
+	hexArray[2] = '\0';
+
+	SimpleString__append__String(string, hexArray);
+}
+
+char SimpleStringg__removeLast(SimpleString* string) {
+	char character = string->string[string->count - 1];
+	SimpleString__reallocIfNeed(string, -1);
+	string->string[string->count - 1] = '\0';
+
+	return character;
+}
+
+void SimpleStringg__removeAll(SimpleString* string) {
+	string->count = 0;
+	string->chunk = 1;
+
+	free(string->string);
+	string->string = (char*)malloc(ALLOC_INTERVAL * string->chunk);
+	assert(string->string);
+
+	string->string[0] = '\0';
+}
+
+void SimpleStringg__uppercase(SimpleString* string) {
+	char character;
+
+	for (int i = 0; i < string->count; ++i) {
+		character = string->string[i];
+
+		if ('a' <= character && character <= 'z') {
+			character += - 'a' + 'A';
+			string->string[i] = character;
+		}
+	}
+}
+
+void SimpleString__lowercase(SimpleString* string) {
+	char character;
+
+	for (int i = 0; i < string->count; ++i) {
+		character = string->string[i];
+
+		if ('A' <= character && character <= 'Z') {
+			character += - 'A' + 'a';
+			string->string[i] = character;
+		}
+	}
+}
 
 
 
@@ -34,39 +135,53 @@ SimpleString* allocSimpleString() {
 	return simpleString;
 }
 
-SimpleString* newSimpleString__shallowMemberwise(char* string, int count) {
+SimpleString* newSimpleString__memberwise(char* string, int count) {
 	SimpleString* simpleString = allocSimpleString();
 
-	simpleString->string = string;
 	simpleString->count = count;
+	simpleString->chunk = (count + 1) / ALLOC_INTERVAL + 1;
+
+	simpleString->string = string;
 
 	return simpleString;
 }
 
-SimpleString* newSimpleString__deepMemberwise(char* string, int count) {
+SimpleString* newSimpleString__designated(char* string, int count) {
 	SimpleString* simpleString = allocSimpleString();
 
-	simpleString->string = (char*)malloc(sizeof(char) * count + 1);
-	strcpy(simpleString->string, string);
 	simpleString->count = count;
+	simpleString->chunk = (count + 1) / ALLOC_INTERVAL + 1;
+
+	simpleString->string = (char*)malloc(ALLOC_INTERVAL * simpleString->chunk);
+	assert(simpleString->string);
+
+	if (string)
+		strcpy(simpleString->string, string);
+	else
+		simpleString->string[0] = '\0';
 
 	return simpleString;
 }
 
 SimpleString* newSimpleString() {
-	SimpleString* simpleString = allocSimpleString();
-
-	simpleString->string = NULL;
-	simpleString->count = 0;
-
-	return simpleString;
+	return newSimpleString__designated(NULL, 0);
 }
 
-void freeSimpleString__shallow(SimpleString* simpleString) {
+SimpleString* newSimpleString__String(char* string) {
+	if (string == NULL) return newSimpleString();
+
+	return newSimpleString__designated(string, strlen(string));
+}
+
+SimpleString* newSimpleString__SimpleString(SimpleString* simpleString) {
+	return newSimpleString__designated(simpleString->string, simpleString->count);
+}
+
+void freeSimpleString__memberwise(SimpleString* simpleString) {
 	free(simpleString);
 }
 
-void freeSimpleString__deep(SimpleString* simpleString) {
+void freeSimpleString__designated(SimpleString* simpleString) {
 	free(simpleString->string);
 	free(simpleString);
 }
--- Project in progress/DataProcessor/SimpleString.h
+++ Project in progress/DataProcessor/SimpleString.h
@@ -1,7 +1,7 @@
 #pragma once
 //********************************************
 // char* title = "SimpleString.h"
-// made by Lieman at 2020.07.08
+// made by Lieman at 2020.07.09
 //
 // description:
 //	SimpleString interface
@@ -19,16 +19,36 @@
 
 // structure
 typedef struct SimpleString {
-	char* string;
 	int count;
+	int chunk;
+	char* string;
 } SimpleString;
 
 
 
 
 
+// static variable
+static const int ALLOC_INTERVAL;
+
+
+
+
+
 // static method
+static int SimpleString__reallocIfNeed(SimpleString* string, int countDelta);
+
 // method
+void SimpleString__append__Character(SimpleString* string, char character);
+void SimpleString__append__String(SimpleString* simpleString, char* string);
+void SimpleString__append__SimpleString(SimpleString* lhs, SimpleString* rhs);
+void SimpleString__append__hex__fromUCharacter(SimpleString* string, unsigned char character);
+
+char SimpleStringg__removeLast(SimpleString* string);
+void SimpleStringg__removeAll(SimpleString* string);
+
+void SimpleString__uppercase(SimpleString* string);
+void SimpleString__lowercase(SimpleString* string);
 
 
 
@@ -37,9 +57,15 @@ typedef struct SimpleString {
 // SimpleString factory method
 SimpleString* allocSimpleString();
 
-SimpleString* newSimpleString__shallowMemberwise(char* string, int count);
-SimpleString* newSimpleString__deepMemberwise(char* string, int count);
+SimpleString* newSimpleString__memberwise(char* string, int count);
+SimpleString* newSimpleString__designated(char* string, int count);
+
 SimpleString* newSimpleString();
 
-void freeSimpleString__shallow(SimpleString* simpleString);
-void freeSimpleString__deep(SimpleString* simpleString);
+SimpleString* newSimpleString__String(char* string);
+SimpleString* newSimpleString__SimpleString(SimpleString* simpleString);
+
+char SimpleStringg__removeLast(SimpleString* string);
+
+void freeSimpleString__memberwise(SimpleString* simpleString);
+void freeSimpleString__designated(SimpleString* simpleString);
