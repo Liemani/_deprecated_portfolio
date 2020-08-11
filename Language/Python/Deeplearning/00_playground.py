a4c10a729af87ed038ebb5001bd8e52582d2700e
Liemani <parkjh2768@naver.com>
2020-08-11 09:05:27 +0900

    Deep learning study

--- Language/Python/Deeplearning/00_playground.py
+++ Language/Python/Deeplearning/00_playground.py
@@ -6,8 +6,56 @@ import numpy as np
 # print("keras version =", k.__version__)
 
 
+def test00():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
 
-        print("Input value: " + str((x1, x2)) + " output value: " + str(y))
+    df = pd.read_csv("pima-indians-diabetes.csv", names=["pregnant", "plasma", "pressure", "thickness", "insulin", "BMI", "pedigree", "age", "class"])
+
+    print(df.head(5))
+
+    print(df.info())
+
+    print(df.describe())
+
+    print(df[["plasma", "class"]])
+
+    colormap = plt.cm.gist_heat
+    plt.figure(figsize=(12, 12))
+
+    sns.heatmap(df.corr(), linewidths=0.1, vmax=0.5, cmap=colormap, linecolor="white", annot=True)
+    plt.show()
+
+    grid = sns.FacetGrid(df, col="class")
+    grid.map(plt.hist, "plasma", bins=10)
+    plt.show()
+
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import Dense
+
+    import numpy
+    import tensorflow as tf
+
+    numpy.random.seed(3)
+    tf.random.set_seed(3)
+
+    dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+    x = dataset[:, 0:8]
+    y = dataset[:, 8]
+
+    model = Sequential()
+    model.add(Dense(12, input_dim=8, activation="relu"))
+    model.add(Dense(8, activation="relu"))
+    model.add(Dense(1, activation="sigmoid"))
+
+    model.compile(loss="binary_crossentropy",
+                  optimizer="adam",
+                  metrics=["accuracy"])
+
+    model.fit(x, y, epochs=200, batch_size=10)
+
+    print("\n Accuracy: %.4f" % (model.evaluate(x, y)[1]))
 
 
 if __name__ == "__main__":
--- Language/Python/Example/00_playground.py
+++ Language/Python/Example/00_playground.py
@@ -219,5 +219,227 @@ def test19():
     webbrowser.open("http://computing.or.kr")
 
 
+def test20():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+
+    python_url = urlopen("http://python.org")
+    soup = BeautifulSoup(python_url.read(), "html.parser")
+    print(soup.h1)
+    print()
+    print(soup)
+
+
+def test21():
+    import urllib.request
+
+    url = "http://storage.googleapis.com/patents/grant_full_text/2014/ipg140107.zip"
+
+    print("Start Download")
+    fname, header = urllib.request.urlretrieve(url, 'ipg140107.zip')
+
+    print("End download")
+
+
+def test22():
+    from bs4 import BeautifulSoup
+
+    with open("example.html") as file:
+        soup = BeautifulSoup(file, "html.parser")
+
+    target = soup.find("div")
+    print(target)
+
+
+def test23():
+    from bs4 import BeautifulSoup
+
+    with open("example.html") as page:
+        soup = BeautifulSoup(page, "html.parser")
+
+    target = soup.find_all("div")
+    print(target)
+
+
+def test24():
+    from bs4 import BeautifulSoup
+
+    with open("example.html") as page:
+        soup = BeautifulSoup(page, "html.parser")
+
+    target = soup.find_all("div", {"id": "ABC_id"})
+    print(target)
+
+
+def test25():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+
+    naver_url = urlopen("https://movie.naver.com/movie/sdb/rank/rmovie.nhn")
+    soup = BeautifulSoup(naver_url.read(), "html.parser")
+    print(soup.title)
+
+
+def test26():
+    from urllib.request import urlopen
+    from bs4 import BeautifulSoup
+
+    naver_url = urlopen("https://movie.naver.com/movie/sdb/rank/rmovie.nhn")
+    soup = BeautifulSoup(naver_url.read(), "html.parser")
+    movie_list = soup.find_all("div", "tit3")
+    print(movie_list)
+
+    for movie in movie_list:
+        print(movie.find("a").text)
+
+    for i, movie in enumerate(movie_list):
+        print("[%d] %s" % (i, movie.find("a").text))
+
+
+def test27():
+    from bs4 import BeautifulSoup as bs
+    from urllib.request import urlopen
+    from urllib.parse import quote_plus
+    baseUrl = "https://search.naver.com/search.naver?where=image&sm=tab_jum&query="
+    plusUrl = input("Input search: ")
+    crawl_num = int(input("Input crawl_num(max: 50): "))
+
+    url = baseUrl + quote_plus(plusUrl)
+    html = urlopen(url)
+    soup = bs(html, "html.parser")
+    img = soup.find_all(class_="_img")
+
+    n = 1
+    for i in img:
+        print(n)
+        imgUrl = i["data-source"]
+        with urlopen(imgUrl) as f:
+            with open("images/img" + str(n)+".jpg", "wb") as h:
+                img = f.read()
+                h.write(img)
+            n += 1
+            if n > crawl_num:
+                break
+
+        print("Image Crawling is done.")
+
+
+def test28():
+    import numpy as np
+
+    np_array = np.array([(1, 2, 3), (4, 5, 6), (7, 8, 9)], dtype=float)
+    print(np_array)
+
+    a = np.full((2, 3), 99, dtype=float)
+    b = np.ones_like(a)
+    print(b)
+
+
+def test29():
+    import numpy as np
+
+    a = np.arange(0, 10, 5)
+    b = np.linspace(0, 10, 5)
+    print(a, b)
+
+
+def test30():
+    import numpy as np
+
+    a = np.random.normal(15, 1, (2, 4))
+    print(a)
+
+
+def test31():
+    import matplotlib.pyplot as plt
+
+    sqr = [i ** 2 for i in range(14)]
+    my_number = [i * 10 for i in range(14)]
+
+    plt.plot(sqr, linewidth=10)
+    plt.plot(my_number, linewidth=5)
+
+    plt.title("0 to 13 Square Numbers")
+    plt.xlabel("This is x label")
+    plt.ylabel("This is y label")
+
+    plt.legend(["SQR", "My Number"])
+
+    plt.show()
+
+
+def test32():
+    import matplotlib.pyplot as plot
+
+    x_data = range(1, 11)
+    y_data = range(10, 0, -1)
+
+    colormap = x_data
+    plot.scatter(x_data, y_data, s=50, c=colormap)
+    plot.title("Scatter makes scatter graph")
+    plot.xlabel("This is x label")
+    plot.ylabel("This is y label")
+    plot.colorbar()
+
+    plot.show()
+
+
+def test33():
+    import matplotlib.pyplot as plot
+
+    x_data = range(6)
+    y_data = [100, 120, 130, 140, 80, 150]
+
+    plot.bar(x_data, y_data, width=0.5, color="red")
+
+    plot.show()
+
+
+def test34():
+    import matplotlib.pyplot as plot
+    import numpy as np
+
+    x_data = range(1, 5)
+    a = np.array([10, 15, 20, 25])
+    b = np.array([50, 55, 60, 65])
+    c = np.array([100, 105, 110, 115])
+
+    plot.bar(x_data, a, color="pink")
+    plot.bar(x_data, b, color="green", bottom=a)
+    plot.bar(x_data, c, color="r", bottom=a+b)
+
+    plot.show()
+
+
+def test35():
+    import matplotlib.pyplot as plot
+
+    ratio = [25, 25, 25, 25]
+
+    plot.pie(ratio)
+
+    plot.show()
+
+
+def test36():
+    import matplotlib.pyplot as plot
+    import numpy as np
+
+    a = np.random.randn(100000)
+    plot.hist(a, bins=30)
+
+    plot.show()
+
+
+def tst37():
+    import matplotlib.pyplot as plot
+    import numpy as np
+
+    a = np.random.normal(10, 3, 5000)
+    plot.hist(a, bins=30, color="pink")
+
+    plot.show()
+
+
 if __name__ == "__main__":
-    test19()
+    tst37()
